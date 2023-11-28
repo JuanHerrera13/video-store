92a605d089674b08a05c17b5463e305ef5726448
Juan Herrera <jm02.herrera@hotmail.com>
2023-11-28 15:49:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Video Store 
+
+That's my Video Store project!
+
+A project developed by me, Juan Herrera, with the purpose to study and improve my knowledge around Java, Spring and 
+good practices.
---
+++ src/test/java/com/video/store/videostore/VideoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.store.videostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
